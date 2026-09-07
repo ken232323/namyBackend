569510da6e95f2022fde9d0984a5ca16fb50df36
ken <kenwonder0@gmail.com>
2026-09-07 14:45:02 +0200

    fix railway config

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`NAMY backend running at http://localhost:${PORT}`);
 });
